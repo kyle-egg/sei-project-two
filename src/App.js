c104c0eeac46361c4e7b40cfee581011f0c0e6ab
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-08-06 14:58:28 +0100

    final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 
 function App() {
-  const [data, setData] = React.useState(null)
+  let [data, setData] = React.useState(null)
   const scoreDisplay = document.querySelector('#score-display')
   let score = 0
 
@@ -19,31 +19,71 @@ function App() {
   }, [ ])
   // console.log(data)
 
+  function handleRefresh() {
+    location.reload()
+  }
+  
   function findSpecial() {
     console.log(data)
-    // data && data.map(clense => {
-    //   // console.log(clense)
-    //   if (data && clense.question.includes('&quot;')) {
-    //     data = clense.question.replaceAll('&quot;', '"')
-    //     console.group(data)
-    //   }
+    data && data.map(clense => {
+      console.log(clense.question)
+      if (data && clense.question.includes('&quot;')) {
+        clense.question = clense.question.replaceAll('&quot;', '"')
+        console.log(data)
+      }
       
-    //   if (data && clense.question.includes('&#039;')) {
-    //     // eslint-disable-next-line quotes
-    //     data = clense.question.replaceAll('&#039;', "'")
-    //     console.group(data)
-    //   }
-    //   if (data && data.includes('&eacute;')) {
-    //     data = data.replaceAll('&eacute;', 'é')
-    //   }
-    //   if (data && data.includes('&amp;')) {
-    //     data = data.replaceAll('&amp;', '&')
-    //   }
-    //   if (data && data.includes('&shy;')) {
-    //     data = data.replaceAll('&shy;', '')
-    //   }
-    // }
-    // )
+      if (data && clense.question.includes('&#039;')) {
+        // eslint-disable-next-line quotes
+        clense.question = clense.question.replaceAll('&#039;', "'")
+        console.log(data)
+      }
+      if (data && clense.question.includes('&eacute;')) {
+        clense.question = clense.question.replaceAll('&eacute;', 'é')
+      }
+      if (data && clense.question.includes('&amp;')) {
+        clense.question = clense.question.replaceAll('&amp;', '&')
+      }
+      if (data && clense.question.includes('&shy;')) {
+        clense.question = clense.question.replaceAll('&shy;', '')
+      }
+      if (data && clense.incorrect_answers.includes('&quot;')) {
+        clense.incorrect_answers = clense.incorrect_answers.replaceAll('&quot;', '"')
+        console.log(data)
+      }
+      if (data && clense.incorrect_answers.includes('&#039;')) {
+        // eslint-disable-next-line quotes
+        clense.incorrect_answers = clense.incorrect_answers.replaceAll('&#039;', "'")
+        console.log(data)
+      }
+      if (data && clense.incorrect_answers.includes('&eacute;')) {
+        clense.incorrect_answers = clense.incorrect_answers.replaceAll('&eacute;', 'é')
+      }
+      if (data && clense.incorrect_answers.includes('&amp;')) {
+        clense.incorrect_answers = clense.incorrect_answers.replaceAll('&amp;', '&')
+      }
+      if (data && clense.incorrect_answers.includes('&shy;')) {
+        clense.incorrect_answers = clense.incorrect_answers.replaceAll('&shy;', '')
+      }
+      if (data && clense.correct_answer.includes('&quot;')) {
+        clense.correct_answer = clense.correct_answer.replaceAll('&quot;', '"')
+        console.log(data)
+      }
+      if (data && clense.correct_answer.includes('&#039;')) {
+        // eslint-disable-next-line quotes
+        clense.correct_answer = clense.correct_answer.replaceAll('&#039;', "'")
+        console.log(data)
+      }
+      if (data && clense.correct_answer.includes('&eacute;')) {
+        clense.correct_answer = clense.correct_answer.replaceAll('&eacute;', 'é')
+      }
+      if (data && clense.correct_answer.includes('&amp;')) {
+        clense.correct_answer = clense.correct_answer.replaceAll('&amp;', '&')
+      }
+      if (data && clense.correct_answer.includes('&shy;')) {
+        clense.correct_answer = clense.correct_answer.replaceAll('&shy;', '')
+      }
+    }
+    )
   }
   
 
@@ -81,6 +121,7 @@ function App() {
           </Route>
           <Route path="/quiz">
             <section className="section">
+              {console.log(data)}
               <div className="container is-max-desktop">
                 <h2 className="title score">Your Score: <span id="score-display"></span></h2>
                 {data && data.map(ques => (
@@ -97,6 +138,8 @@ function App() {
                   </div>
                 ))}
               </div>
+              <button className="button" onClick={handleRefresh}>Give Me More Questions!</button>
+
             </section>
           </Route>
         </Switch>
